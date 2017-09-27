cbedfef8e2d6f5dc74efc0b346db39a04d25f725
lorddorn <pedrotelles@gec.inatel.br>
2017-09-27 19:57:28 -0300

    updated runserver.py

--- runserver.py
+++ runserver.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     environ['TZ'] = 'America/Sao_Paulo'
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '80'))
+        PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
-        PORT = 80
+        PORT = 5555
     app.run(HOST, PORT)
